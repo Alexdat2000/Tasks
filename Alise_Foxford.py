5c962fafc880a22187d683c2c83a075ce1325530
Alexdat2000 <alexdat@list.ru>
2018-03-24 08:36:00 +0300

    Add files via upload

---
+++ Alise_Foxford.py
@@ -0,0 +1,72 @@
+file = open('database.txt')
+m = []
+for line in file.readlines():
+	m.append(int(line))
+
+print(m)
+
+if 52 in m:
+	print(52, True)
+else:
+	print(52,False)
+
+if 2 in m:
+	print(2, True)
+else:
+	print(2, False)
+
+if 5 in m:
+	print(5, True)
+else:
+	print(5,False)
+
+if 113 in m:
+	print(113, True)
+else:
+	print(113, False)
+
+if 2018 in m:
+	print(2018, True)
+else:
+	print(2018, False)
+
+if 58 in m:
+	print(58, True)
+else:
+	print(58,False)
+
+if 3 in m:
+	print(3, True)
+else:
+	print(3,False)
+
+if 6 in m:
+	print(6, True)
+else:
+	print(6, False)
+
+if 7 in m:
+	print(7, True)
+else:
+	print(7, False)
+
+if 1 in m:
+	print(1, True)
+else:
+	print(1, False)
+
+if 80 in m:
+	print(80, True)
+else:
+	print(80, False)
+
+if 101 in m:
+	print(101, True)
+else:
+	print(101, False)
+
+
+
+
+		
+
